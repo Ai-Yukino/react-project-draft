173dc58c6e6f09501c49b85912e888b307591cb3
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2021-11-22 17:46:25 -0600

    🔥 Clean up

--- tea/src/App.jsx
+++ tea/src/App.jsx
@@ -1,11 +1,9 @@
 import Products from "./components/Products";
-import { Link } from "react-router-dom";
 import "./index.css";
 
 function App() {
   return (
     <div>
-      <Link to="/test">Test</Link>
       <Products />
     </div>
   );
