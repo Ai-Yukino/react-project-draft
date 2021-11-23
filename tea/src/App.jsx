ed35503a55630c5c31a35e8e315aa641cfb71668
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2021-11-22 23:43:59 -0600

    Added <Link> without rendering

--- tea/src/App.jsx
+++ tea/src/App.jsx
@@ -1,3 +1,16 @@
+// import { Outlet } from "react-router-dom";
+// import "./index.css";
+
+// function App() {
+//   return (
+//     <div>
+//       <Outlet />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import Products from "./components/Products";
 import "./index.css";
 
